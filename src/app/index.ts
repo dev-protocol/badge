a33984af7e0129c81408d9a77eb402087637c1d5
aggre <hiroyuki.aggre@gmail.com>
2018-10-29 12:48:53 +0900

    Supports empty response

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,7 +4,8 @@ import { svg } from './svg'
 
 export const app = micro(async (req, res) => {
 	const balance = await fetchBalance(req)
-	// tslint:disable-next-line:no-expression-statement
-	res.setHeader('Content-Type', 'image/svg+xml')
-	return svg(balance)
+	const body = svg(balance)
+	// tslint:disable-next-line:no-expression-statement no-unused-expression
+	body && res.setHeader('Content-Type', 'image/svg+xml')
+	return body
 })
