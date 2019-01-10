7f89bb0b5e2c57692ea0c3ce10f9895173d69ea5
aggre <hiroyuki.aggre@gmail.com>
2019-01-10 10:21:17 +0900

    add cache-control to response headers

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,6 +6,11 @@ export const app = async (req: IncomingMessage, res: ServerResponse) => {
 	const balance = await fetchBalance(req)
 	const body = svg(balance)
 	// tslint:disable-next-line:no-expression-statement no-unused-expression
-	body && res.setHeader('Content-Type', 'image/svg+xml')
+	body &&
+		res.setHeader('content-type', 'image/svg+xml') &&
+		res.setHeader(
+			'cache-control',
+			'public, s-maxage=86400, stale-while-revalidate=3600, must-revalidate'
+		)
 	return res.end(body)
 }
