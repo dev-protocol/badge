c593c7734ccf261727d16181f26139eb48700374
aggre <hiroyuki.aggre@gmail.com>
2020-09-03 11:25:49 +0900

    change route

--- src/app/fetch-balance.ts
+++ src/app/fetch-balance.ts
@@ -3,12 +3,6 @@ import { parse } from 'url'
 import { gql } from 'graphql-request'
 import fetch from 'node-fetch'
 
-interface IAuthenticationResult {
-	readonly data: {
-		readonly property_authentication: readonly { readonly property: string }[]
-	}
-}
-
 interface IPropertyLockedUpResult {
 	readonly data: {
 		readonly property_lockup_sum_values: readonly {
@@ -20,32 +14,8 @@ interface IPropertyLockedUpResult {
 
 const GRAPHQL_BASE_URL = 'https://api.devprtcl.com/v1/graphql'
 
-const getPackageNamePath = (req: IncomingMessage) =>
+const getAddressFromPath = (req: IncomingMessage) =>
 	((parsed) => (parsed.pathname || '').replace(/^\//, ''))(parse(req.url || ''))
-
-const getPropertyFromName = async (name: string) => {
-	const authenticationQuery = gql`
-		query getPropertyFromName($name: String) {
-			property_authentication(where: { authentication_id: { _eq: $name } }) {
-				authentication_id
-				property
-			}
-		}
-	`
-	return fetch(GRAPHQL_BASE_URL, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-		},
-		body: JSON.stringify({
-			query: authenticationQuery,
-			variables: { name },
-		}),
-	})
-		.then((r) => r.json())
-		.then((data: IAuthenticationResult) => data)
-}
 
 const getBalanceByPropertyAddress = async (propertyAddress: string) => {
 	const propertyLockedQuery = gql`
@@ -78,14 +48,12 @@ const getBalanceByPropertyAddress = async (propertyAddress: string) => {
 }
 
 export const fetchBalance = async (req: IncomingMessage) => {
-	const name = getPackageNamePath(req)
-	const result = await getPropertyFromName(name)
-	const address = result?.data?.property_authentication?.[0]?.property
-	const etherscan = address
+	const address = getAddressFromPath(req)
+	const balance = address
 		? await getBalanceByPropertyAddress(address)
 		: undefined
-	return etherscan
-		? etherscan?.data?.property_lockup_sum_values?.[0]?.sum_values /
+	return balance
+		? balance?.data?.property_lockup_sum_values?.[0]?.sum_values /
 				Math.pow(10, 18)
 		: 0
 }
