748962a2b81582015dd8d0d85f45d3103872f155
aggre <hiroyuki.aggre@gmail.com>
2020-09-03 11:45:30 +0900

    fix broken tests

--- src/app/fetch-balance.test.ts
+++ src/app/fetch-balance.test.ts
@@ -1,38 +1,45 @@
 import { fetchBalance } from './fetch-balance'
 import { IncomingMessage } from 'http'
 import { strictEqual } from 'assert'
-import { getBalanceDev } from 'dev-distribution/src/libs'
+import { stub } from 'sinon'
+import * as fetch from 'node-fetch'
 
 // tslint:disable:no-expression-statement no-http-string
 
 // tslint:disable-next-line: no-object-literal-type-assertion
 const mockRequest = (url: string) => ({ url } as IncomingMessage)
 
+stub(fetch, 'default').callsFake(
+	async (_url: any, _init: any) =>
+		({
+			json: async () => {
+				return Promise.resolve({
+					data: {
+						property_lockup_sum_values: [
+							{
+								sum_values: '123456000000000000000000',
+								property_address: '0x0',
+							},
+						],
+					},
+				})
+			},
+		} as any)
+)
+
 describe('fetch-balance', () => {
 	describe('Get the number of tokens for the specified address', () => {
 		it('Valid address', async () => {
-			const balance = await fetchBalance(mockRequest('http://localhost/chalk'))
-			const expected = await getBalanceDev(
-				'0xfb5c0e6400a95e4191d46196073d30a289abd15c'
+			const balance = await fetchBalance(
+				mockRequest(
+					'http://localhost/0xfb5c0e6400a95e4191d46196073d30a289abd15c'
+				)
 			)
-			strictEqual(balance, expected.balance)
+			strictEqual(balance, 123456)
 		}).timeout(10000)
 
 		it('Invalid address', async () => {
-			strictEqual(
-				await fetchBalance(mockRequest('http://localhost/')),
-				undefined
-			)
-
-			strictEqual(
-				await fetchBalance(mockRequest('http://localhost/xxx')),
-				undefined
-			)
-
-			strictEqual(
-				await fetchBalance(mockRequest('http://localhost/n')),
-				undefined
-			)
+			strictEqual(await fetchBalance(mockRequest('http://localhost/')), 0)
 		})
 	})
 })
