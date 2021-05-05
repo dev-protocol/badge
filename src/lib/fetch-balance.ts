3f043ada21db6bece8af653b4d0007e9f795cef7
aggre <hiroyuki.aggre@gmail.com>
2021-05-05 17:14:07 +0900

    fix #79

--- src/lib/fetch-balance.ts
+++ src/lib/fetch-balance.ts
@@ -1,49 +1,6 @@
-import { gql } from 'graphql-request'
-import fetch from 'node-fetch'
-import { always } from 'ramda'
-
-type IPropertyLockedUpResult = {
-	readonly data: {
-		readonly property_lockup_sum_values: readonly {
-			readonly sum_values: number
-		}[]
-	}
-}
-
-const GRAPHQL_BASE_URL = 'https://api.devprtcl.com/v1/graphql'
-
-const getBalanceByPropertyAddress = async (
-	propertyAddress: string
-): Promise<IPropertyLockedUpResult> => {
-	const propertyLockedQuery = gql`
-		query getBalanceByPropertyAddress($address: String) {
-			property_lockup_sum_values(
-				where: { property_address: { _eq: $address } }
-			) {
-				sum_values
-			}
-		}
-	`
-
-	return fetch(GRAPHQL_BASE_URL, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Accept: 'application/json',
-		},
-		body: JSON.stringify({
-			query: propertyLockedQuery,
-			variables: { address: propertyAddress },
-		}),
-	})
-		.then((r) => r.json())
-		.then((data: IPropertyLockedUpResult) => data)
-		.catch(
-			always({
-				data: { property_lockup_sum_values: [{ sum_values: 0 }] },
-			})
-		)
-}
+import Web3 from 'web3'
+import { addresses, contractFactory } from '@devprotocol/dev-kit'
+import BigNumber from 'bignumber.js'
 
 export const fetchBalance = async (
 	addressFromPath?: string | readonly string[]
@@ -53,11 +10,13 @@ export const fetchBalance = async (
 			? addressFromPath
 			: addressFromPath[0]
 		: undefined
-	const fromGQL = address
-		? await getBalanceByPropertyAddress(address)
-		: undefined
-	const balance = fromGQL
-		? fromGQL?.data?.property_lockup_sum_values?.[0]?.sum_values ?? 0
+	const devKit = contractFactory(
+		new Web3.providers.HttpProvider(process.env.JRON_RPC_ENDPOINT!)
+	)
+	const locked = address
+		? await devKit
+				.lockup(await devKit.registry(addresses.eth.main.registry).lockup())
+				.getPropertyValue(address)
 		: 0
-	return balance ? balance / Math.pow(10, 18) : 0
+	return locked ? new BigNumber(locked).div(Math.pow(10, 18)).toNumber() : 0
 }
