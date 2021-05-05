51e12e184b747774ed476adff43e868b69953d1e
aggre <hiroyuki.aggre@gmail.com>
2021-05-05 17:17:34 +0900

    fix #79 (#82)

--- src/lib/fetch-balance.test.ts
+++ src/lib/fetch-balance.test.ts
@@ -1,30 +1,25 @@
+/* eslint-disable functional/no-class */
+/* eslint-disable functional/immutable-data */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { fetchBalance } from './fetch-balance'
 import { strictEqual } from 'assert'
 import { stub } from 'sinon'
-import * as fetch from 'node-fetch'
+import * as devKit from '@devprotocol/dev-kit'
 
-stub(fetch, 'default').callsFake(
-	async (_url: any, _init: any) =>
+stub(devKit, 'contractFactory').callsFake(
+	() =>
 		({
-			json: async () => {
-				return Promise.resolve({
-					data: {
-						property_lockup_sum_values: [
-							{
-								sum_values: '123456000000000000000000',
-								property_address: '0x0',
-							},
-						],
-					},
-				})
-			},
+			registry: () => ({ lockup: async () => '' }),
+			lockup: () => ({
+				getPropertyValue: async () => '123456000000000000000000',
+			}),
 		} as any)
 )
 
 describe('fetch-balance', () => {
 	describe('Get the number of tokens for the specified address', () => {
+		process.env.JRON_RPC_ENDPOINT = 'http://localhost'
 		it('Valid address', async () => {
 			const balance = await fetchBalance(
 				'0xfb5c0e6400a95e4191d46196073d30a289abd15c'
